46e198ac7dfbd743f47b8b83f97c55d0dad4f297
Fran Palmero <sweetydesigns@gmail.com>
2024-07-23 01:31:19 +0200

    Clean Home Component

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -2,33 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartService } from '../services/cart.service';
 import { ProductService } from '../services/product.service';
-import { CartModalComponent } from '../shared/cart-modal.component';
-import { HeaderComponent } from '../header/header.component';
 import { Products } from '../interfaces/products';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CartModalComponent, HeaderComponent],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   products: Products[] = [];
-  cart: Products[] = [];
-  total = 0;
 
   constructor(private cartService: CartService, private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.cartService.getCart().subscribe(cart => {
-      this.cart = cart;
-    });
-
-    this.cartService.getTotal().subscribe(total => {
-      this.total = total;
-    });
-
     this.productService.getProducts().subscribe(products => {
       this.products = products;
     });
@@ -38,7 +26,4 @@ export class HomeComponent implements OnInit {
     this.cartService.buy(id, this.products);
   }
 
-  cleanCart(): void {
-    this.cartService.cleanCart();
-  }
 }
