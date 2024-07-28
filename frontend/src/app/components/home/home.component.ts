10d85f6ec5d8262bb3c2ac5623f1c2695f72983c
Fran Palmero <sweetydesigns@gmail.com>
2024-07-28 16:50:19 +0200

    Home: Loader v0

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartService } from '../../services/cart.service';
 import { DiscogsService } from '../../services/discogs.service';
-
+import { interval } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -13,20 +14,32 @@ import { DiscogsService } from '../../services/discogs.service';
 })
 export class HomeComponent implements OnInit {
   products: any[] = [];
+  progress: number = 0;
+  loading: boolean = true;
 
   constructor(private cartService: CartService, private discogsService: DiscogsService) {}
 
   ngOnInit(): void {
     console.log('Fetching inventory...');
+    this.simulateLoadingProgress();
     this.discogsService.getInventory().subscribe({
       next: data => {
-        this.products = data;
+        this.products = data.listings || [];
+        this.progress = 100;
+        this.loading = false;
       },
       error: error => {
         console.error('Error fetching inventory:', error);
-      },
-      complete: () => {
-        console.log('Inventory fetch complete');
+        this.loading = false;
+      }
+    });
+  }
+
+  simulateLoadingProgress(): void {
+    const interval$ = interval(100); // Incrementa cada 100ms
+    interval$.pipe(takeUntil(interval(1000))).subscribe(() => { // Toma valores hasta 1 segundo
+      if (this.progress < 100) {
+        this.progress += 10; // Incrementa el progreso en 10%
       }
     });
   }
@@ -34,5 +47,4 @@ export class HomeComponent implements OnInit {
   buyProduct(id: number): void {
     this.cartService.buy(id, this.products);
   }
-
 }
--- frontend/src/app/services/discogs.service.ts
+++ frontend/src/app/services/discogs.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpEventType, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { tap, delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DiscogsService {
-  private apiUrl = 'https://api.discogs.com/users/BarcelonaCityRecord/inventory?&per_page=20';
+  private apiUrl = 'https://api.discogs.com/users/BarcelonaCityRecord/inventory?&per_page=40';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getInventory(): Observable<any> {
+    // Simulamos una llamada a la API con un retraso artificial
     return this.http.get<any>(this.apiUrl).pipe(
-      map(response => response.listings || []) // Asegúrate de que estamos accediendo a 'listings'
+      delay(1000), // Simular un retraso de 1 segundo
+      tap(response => response.listings || [])
     );
   }
 }
